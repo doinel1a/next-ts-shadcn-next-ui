96d1e82052f8383eac2e22ff077549234adb580b
doinel1a <doinel1atanasiu@gmail.com>
2024-03-10 17:55:45 +0100

    feat: install & configure next-ui

--- src/providers/root.tsx
+++ src/providers/root.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import type { PropsWithChildren } from 'react';
 
+import NextUiProvider from './next-ui';
 import ThemeProvider from './theme';
 
 type TRootProvider = PropsWithChildren;
 
 export default function RootProvider({ children }: TRootProvider) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <NextUiProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </NextUiProvider>
+  );
 }
