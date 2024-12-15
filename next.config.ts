074384872e8a75e65850b868b62f8e355168ab5a
doinel1a <doinel1atanasiu@gmail.com>
2024-12-15 19:18:43 +0100

    refactor: use .ts file extension in next.config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next';
+
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
   }
-};
+} satisfies NextConfig;
 
 export default nextConfig;
